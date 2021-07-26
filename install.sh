1125a589a9da58405922daf9d721cb38e88ee97a
18235210 <yytamm@sberbank.ru>
2021-07-26 15:48:10 +0300

    говорят виртуалэнв уже установлен

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-pip3 install virtualenv
 python3 -m virtualenv venv
 source ./venv/bin/activate
-# Прописываем откуда брать пакеты и устанавливаем то, что через поэтри не  ставится
+# Прописываем откуда брать пакеты и устанавливаем то, что через поэтри не ставится
 python3 resolve_mirror.py
 # TODO: добавить сборку whl и выкладывания в Nexus
 poetry lock
