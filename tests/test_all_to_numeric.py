181ab53c45e95c789c072d034d8aebed81e5bd8d
monkey0head <aavolodkevich@sberbank.ru>
2022-02-10 23:58:51 +0300

    tests fix

--- tests/test_all_to_numeric.py
+++ tests/test_all_to_numeric.py
@@ -5,6 +5,8 @@ from pyspark.sql import functions as sf
 from replay.data_preparator import ToNumericFeatureTransformer
 
 from tests.utils import (
+    item_features,
+    spark,
     sparkDataFrameEqual,
 )
 
@@ -20,7 +22,7 @@ def test_all_to_numeric_big_threshold(item_features):
     )
     assert sorted(transformed.columns) == [
         "iq",
-        "item_id",
+        "item_idx",
         "ohe_class_cat",
         "ohe_class_mouse",
         "ohe_color_black",
@@ -33,14 +35,14 @@ def test_all_to_numeric_threshold(item_features):
     processor.fit(item_features.filter(sf.col("class") != "dog"))
     transformed = processor.transform(item_features)
     assert "iq" in transformed.columns and "color" not in transformed.columns
-    assert sorted(transformed.columns) == ["iq", "item_id"]
+    assert sorted(transformed.columns) == ["iq", "item_idx"]
 
 
 def test_all_to_numeric_only_numeric(item_features):
     processor = ToNumericFeatureTransformer(threshold=1)
-    processor.fit(item_features.select("item_id", "iq"))
-    transformed = processor.transform(item_features.select("item_id", "iq"))
-    sparkDataFrameEqual(item_features.select("item_id", "iq"), transformed)
+    processor.fit(item_features.select("item_idx", "iq"))
+    transformed = processor.transform(item_features.select("item_idx", "iq"))
+    sparkDataFrameEqual(item_features.select("item_idx", "iq"), transformed)
 
 
 def test_all_to_numeric_numeric_and_greater_threshold(
@@ -49,7 +51,7 @@ def test_all_to_numeric_numeric_and_greater_threshold(
     processor = ToNumericFeatureTransformer(threshold=0)
     processor.fit(item_features)
     transformed = processor.transform(item_features)
-    sparkDataFrameEqual(item_features.select("item_id", "iq"), transformed)
+    sparkDataFrameEqual(item_features.select("item_idx", "iq"), transformed)
 
 
 def test_all_to_numeric_empty(item_features):
@@ -57,7 +59,9 @@ def test_all_to_numeric_empty(item_features):
     processor.fit(None)
     assert processor.transform(item_features) is None
 
-    processor.fit(item_features.select("item_id", "class"))
+    processor.fit(item_features.select("item_idx", "class"))
     assert processor.cat_feat_transformer is None
-    transformed = processor.transform(item_features.select("item_id", "class"))
-    assert transformed.columns == ["item_id"]
+    transformed = processor.transform(
+        item_features.select("item_idx", "class")
+    )
+    assert transformed.columns == ["item_idx"]
